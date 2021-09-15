aa72cce8c0c5e863508221818681d93354eca612
UJONE <59907964+toot09@users.noreply.github.com>
2021-09-15 10:51:07 +0000

    20210915 H-Index. Yesterday. I gave up to having a BB

---
+++ Level2/H-Index.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hindex {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        for(int i=0 ; i<citations.length ; i++) {
+            int h = citations.length-i;
+            if(citations[i]>=h)
+                return h;
+        }
+        return 0;
+    }
+}
