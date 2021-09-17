1cddf8bbecc35116fc0033a8c563948864a23209
UJONE <59907964+toot09@users.noreply.github.com>
2021-09-17 06:18:29 +0000

    구명보트, 점프와 순간이동 Commit

---
+++ Level2/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class gmboat {
+    public int solution(int[] people, int limit) {
+        int answer = 0, i=0, j=people.length-1;
+        Arrays.sort(people);
+        while(i<=j) {
+            if(people[i]+people[j]<=limit) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
