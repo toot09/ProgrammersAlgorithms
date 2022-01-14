146807bb7806df328e11c71439e707eafabef9c9
UJONE <59907964+toot09@users.noreply.github.com>
2022-01-14 06:38:54 +0000

    줄 서는 방법

---
+++ Level3/줄 서는 방법.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class 줄_서는_방법 {
+    public int[] solution(int n, long k) {
+        int[] ret = new int[n];
+		List<Integer> list = new ArrayList<>();
+        long num=1;
+        int idx=0;
+		for(int x=1 ; x<=n ; x++) {
+			list.add(x);
+			num *= x;
+		}
+        k--;
+        while(idx<ret.length) {
+        	num /= (n--);
+        	int i = (int)(k/num);
+        	ret[idx++] = list.get(i);
+        	list.remove(i);
+        	k %= num;
+        }
+        return ret;
+    }
+}
