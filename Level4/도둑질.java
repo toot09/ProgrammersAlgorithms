132bd1b59568af169e030b68c5658c2006e704bc
UJONE <59907964+toot09@users.noreply.github.com>
2022-03-22 11:36:44 +0000

    20220322 Theif

--- Level4/도둑질.java
+++ Level4/도둑질.java
@@ -6,7 +6,7 @@ class 도둑질 {
         dp[0][1] = money[0];
         dp[1][0] = 0;
         dp[1][1] = money[1];
-		
+		 
         for(int i=2; i<len ; i++) {
           dp[0][i] = Math.max(dp[0][i-2]+money[i], dp[0][i-1]);
           dp[1][i] = Math.max(dp[1][i-2]+money[i], dp[1][i-1]);
